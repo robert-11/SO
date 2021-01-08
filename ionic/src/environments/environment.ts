2b21530bb5144a5207eb7bfc211f7c2be713fc55
robert-11 <robert-nicolae.albu@student.unitbv.ro>
2021-01-08 11:58:58 +0200

    v1

---
+++ ionic/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD4El8qwPLCp8oYUb2l0kyQgJzFZ6JuoL8',
+    authDomain: 'ionic-73818.firebaseapp.com',
+    projectId: 'ionic-73818',
+    storageBucket: 'ionic-73818.appspot.com',
+    messagingSenderId: '377008166425',
+    appId: '1:377008166425:web:6f7ed882c1deeb5ddbfc88',
+    measurementId: 'G-SGQZTHNX1G'
+  }
+};
